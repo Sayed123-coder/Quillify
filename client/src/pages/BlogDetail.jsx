79337ef549032211adc2f9926b63eab009c0682f
Sayed Aman <sayedamancoder@gmail.com>
2026-05-23 12:45:43 +0530

    Updated BlogDetail.jsx

--- client/src/pages/BlogDetail.jsx
+++ client/src/pages/BlogDetail.jsx
@@ -205,7 +205,7 @@ useEffect(() => {
             <img
               src={blog.thumbnail}
               alt={blog.title}
-              className="w-full h-64 object-cover"
+              className="w-full object-contain"
               onError={(e) => (e.target.style.display = "none")}
             />
           </div>
@@ -213,8 +213,17 @@ useEffect(() => {
 
         {/* Content */}
         <div
-          className="ql-editor !p-0 text-gray-700 text-sm leading-relaxed"
-          dangerouslySetInnerHTML={{ __html: blog.content }}
+           className="ql-editor !p-0 text-gray-700 text-sm leading-relaxed
+           [&_h1]:text-2xl [&_h1]:font-bold [&_h1]:mb-4 [&_h1]:mt-6
+           [&_h2]:text-xl [&_h2]:font-semibold [&_h2]:mb-3 [&_h2]:mt-5
+           [&_h3]:text-lg [&_h3]:font-semibold [&_h3]:mb-2 [&_h3]:mt-4
+           [&_p]:mb-3 [&_p]:leading-relaxed
+           [&_ul]:list-disc [&_ul]:pl-5 [&_ul]:mb-3
+           [&_ol]:list-decimal [&_ol]:pl-5 [&_ol]:mb-3
+           [&_li]:mb-1
+           [&_strong]:font-semibold
+           [&_blockquote]:border-l-4 [&_blockquote]:border-indigo-300 [&_blockquote]:pl-4 [&_blockquote]:italic [&_blockquote]:text-gray-500"
+           dangerouslySetInnerHTML={{ __html: blog.content }}
         />
          {/* Like Button */}
           <div className="flex items-center gap-3 mt-8">
